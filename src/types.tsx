931bb9a5a310de0d2b23e22c1af840acfe1973f4
elizaveta-tukailo <lizatukailo@gmail.com>
2024-07-05 23:52:10 +0300

    feat: add types for item and state

--- src/types.tsx
+++ src/types.tsx
@@ -1,3 +1,16 @@
 export type InputValue = {
   value: string;
 };
+export type Item = {
+  id?: number;
+  name: string;
+  height: number;
+  hair_color: string;
+  skin_color: string;
+  gender: string;
+};
+export type State = {
+  error: null;
+  isLoaded: boolean;
+  items: Item[];
+};
