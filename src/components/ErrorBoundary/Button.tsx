279dc901cc4cab787da05c2fb4b7218454d99e9c
elizaveta-tukailo <lizatukailo@gmail.com>
2024-07-07 09:25:21 +0300

    feat: add error button

---
+++ src/components/ErrorBoundary/Button.tsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import styles from './ErrorBoundary.module.css';
+
+class ErrorButton extends Component {
+  state = {
+    hasError: true,
+  };
+  handleErrorSubmit = () => {
+    this.setState({ hasError: false });
+  };
+  render() {
+    if (this.state.hasError) {
+      return (
+        <>
+          <form onSubmit={this.handleErrorSubmit}>
+            <button className={styles.error__button} type="submit">
+              Error Button
+            </button>
+          </form>
+        </>
+      );
+    } else {
+      throw new Error('Error');
+    }
+  }
+}
+
+export default ErrorButton;
