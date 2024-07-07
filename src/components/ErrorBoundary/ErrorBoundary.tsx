72ee2c61ab6b72270088ab90237a948033756dd0
elizaveta-tukailo <lizatukailo@gmail.com>
2024-07-07 09:24:55 +0300

    feat: add error boundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+import ErrorPage from '../../pages/ErrorPage';
+
+interface Props {
+  children?: ReactNode;
+}
+export interface HasState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, HasState> {
+  public state: HasState = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(): HasState {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <ErrorPage />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
