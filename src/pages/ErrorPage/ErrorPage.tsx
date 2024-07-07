85b2f9556be02f41f7e63154efed58e2b9d6e833
elizaveta-tukailo <lizatukailo@gmail.com>
2024-07-07 09:26:11 +0300

    feat: add error page

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import styles from './ErrorPage.module.css';
+
+class ErrorPage extends Component {
+  handleResetSubmit = (): void => {
+    localStorage.setItem('searchQuery', '');
+  };
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleResetSubmit}>
+          <div className={styles.container}>
+            <h1>Error</h1>
+            <button className={styles.reset} type="submit">
+              Reset
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
+
+export default ErrorPage;
