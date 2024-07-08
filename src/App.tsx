04bd9260844d51c6464c4f283264474b15592a25
Elizaveta <lizatukailo@gmail.com>
2024-07-08 22:58:34 +0300

    feat: add route for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import ErrorBoundary from './components/ErrorBoundary';
 import MainPage from './pages/MainPage';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
       <ErrorBoundary>
-        <MainPage />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
       </ErrorBoundary>
     </>
   );
