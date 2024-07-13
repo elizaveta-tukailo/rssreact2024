a2ac83d605415eb332c9be8028a1b5c5e7fa49e4
Elizaveta <lizatukailo@gmail.com>
2024-07-13 20:15:22 +0300

    feat: change to functional component

--- src/components/ErrorBoundary/Button.tsx
+++ src/components/ErrorBoundary/Button.tsx
@@ -1,28 +1,25 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './error-boundary.module.css';
 
-class ErrorButton extends Component {
-  state = {
-    hasError: true,
+const ErrorButton = () => {
+  const [hasError, sethasError] = useState(true);
+
+  const handleErrorSubmit = () => {
+    sethasError(false);
   };
-  handleErrorSubmit = () => {
-    this.setState({ hasError: false });
-  };
-  render() {
-    if (this.state.hasError) {
-      return (
-        <>
-          <form onSubmit={this.handleErrorSubmit}>
-            <button className={styles.error__button} type="submit">
-              Error Button
-            </button>
-          </form>
-        </>
-      );
-    } else {
-      throw new Error('Error');
-    }
+  if (hasError) {
+    return (
+      <>
+        <form onSubmit={handleErrorSubmit}>
+          <button className={styles.error__button} type="submit">
+            Error Button
+          </button>
+        </form>
+      </>
+    );
+  } else {
+    throw new Error('Error');
   }
-}
+};
 
 export default ErrorButton;
