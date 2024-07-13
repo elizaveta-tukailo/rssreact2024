ed7fb95e904621f48423ed3cbc3b16ff336694a0
Elizaveta <lizatukailo@gmail.com>
2024-07-13 20:08:16 +0300

    fix: change layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header';
+import styles from './layout.module.css';
 
 const Layout: React.FC = () => {
   return (
-    <>
+    <div className={styles['page__wrap']}>
       <Header />
-      <Outlet />
-    </>
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
   );
 };
 export default Layout;
