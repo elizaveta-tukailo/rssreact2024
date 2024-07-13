75bfb6aafaf88f09f0725e31c6e9a70e1959f878
Elizaveta <lizatukailo@gmail.com>
2024-07-13 19:58:49 +0300

    feat: change header structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,22 @@
-import ErrorButton from '../ErrorBoundary/Button';
-import Search from '../Search';
+import { NavLink } from 'react-router-dom';
+import styles from './header.module.css';
+import PathesEnum from '../../common/enums/pathes';
+
 const Header: React.FC = () => {
   return (
-    <header>
-      <ErrorButton />
-      <Search />
+    <header className={styles['header']}>
+      <div className="container">
+        <nav className={styles['header__nav']}>
+          <NavLink
+            to={PathesEnum.home}
+            className={({ isActive }) =>
+              isActive ? styles['link-active'] : styles['link']
+            }
+          >
+            Home
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 };
