c86a6c3390e5482f3ceb0e62a9ab4a07c8c20f4a
Elizaveta <lizatukailo@gmail.com>
2024-07-13 20:01:23 +0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import ErrorBoundary from './components/ErrorBoundary';
 import MainPage from './pages/main-page';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import NotFoundPage from './pages/not-found-page';
+import Layout from './components/Layout';
+import Card from './components/Card';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App: React.FC = () => {
   return (
-    <>
-      <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </ErrorBoundary>
-    </>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<MainPage />}>
+            <Route path="details/:peopleId" element={<Card />} />
+          </Route>
+          <Route path="page/:id" element={<MainPage />}>
+            <Route path="details/:peopleId" element={<Card />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 };
 export default App;
