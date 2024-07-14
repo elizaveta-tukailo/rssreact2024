eb0fb1f1c0761cdcc7ecf42f4ba29eb715877448
Elizaveta <lizatukailo@gmail.com>
2024-07-14 11:13:18 +0300

    feat: add state and change card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,72 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import ICard from '../../interfaces/ICard';
 import styles from './card.module.css';
+import Loader from '../Loader';
+import { BASE_URL } from '../../common/constants';
 
-interface CardProps {
-  card: ICard | null;
-}
+const Card = () => {
+  const { peopleId } = useParams();
+  const [card, setCard] = useState<ICard | null>(null);
+  const navigate = useNavigate();
+  const clickCloseModal = () => {
+    navigate(-1);
+    localStorage.setItem('active', '');
+  };
+  const cardInfo = {
+    item: card,
+    isLoaded: false,
+  };
+  const [stateCard, setStateCard] = useState(cardInfo);
+  useEffect(() => {
+    fetch(`${BASE_URL}/${peopleId}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setCard(result);
+        setStateCard({
+          isLoaded: true,
+          item: result,
+        });
+      });
+  }, [peopleId]);
 
-export default function Card({ card }: CardProps) {
-  return (
-    <div className={styles.people__item}>
-      <div className={styles.item__name}>{card?.name}</div>
-      <div className={styles.item__info}>
-        <div>Height: {card?.height}</div>
-        <div>Hair color: {card?.hair_color}</div>
-        <div>Skin color: {card?.skin_color}</div>
-        <div>Gender: {card?.gender} </div>
-      </div>
-    </div>
-  );
-}
+  if (stateCard) {
+    const isLoaded = stateCard.isLoaded;
+    if (!isLoaded) {
+      return <Loader />;
+    } else if (card && card.location && card.created) {
+      return (
+        <div className={styles['card__item-wrap']}>
+          <div className={styles['card__item']}>
+            <div className={styles['card__item-id']}>Card ID: {card.id}</div>
+            <h3 className={styles['card__item-title']}> {card.name} </h3>
+            <button
+              className={styles['card__item-btn']}
+              onClick={clickCloseModal}
+            ></button>
+          </div>
+          <div className={styles['card__item-row']}>
+            <div className={styles['card__item-image']}>
+              <img src={card.image}></img>
+            </div>
+            <div className={styles['card__item-info']}>
+              <div className={styles['card__item-info-item']}>
+                <span>Species:</span> {card.species}
+              </div>
+              <div className={styles['card__item-info-item']}>
+                <span>Status:</span> {card.status}
+              </div>
+              <div className={styles['card__item-info-item']}>
+                <span> Gender:</span> {card.gender}
+              </div>
+              <div className={styles['card__item-info-item']}>
+                <span>Planet:</span> {card.location.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+};
+export default Card;
