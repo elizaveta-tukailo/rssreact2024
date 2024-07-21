71314a28fe22902c374a07258dd0af48d0021963
Elizaveta <lizatukailo@gmail.com>
2024-07-21 23:40:41 +0300

    feat: add theme classes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { useTheme } from '../../context/ThemeContext';
 import Header from '../Header';
 import styles from './layout.module.css';
 
 const Layout: React.FC = () => {
+  const { theme } = useTheme();
+  const themeClass =
+    theme === 'dark' ? styles['page__wrap--dark'] : styles['page__wrap--light'];
   return (
-    <div className={styles['page__wrap']}>
+    <div className={`${styles['page__wrap']} ${themeClass}`}>
       <Header />
       <div className="container">
         <Outlet />
