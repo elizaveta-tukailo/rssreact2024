984df8bd4c7c746c571c97e477d6b7c4abd87637
Elizaveta <lizatukailo@gmail.com>
2024-07-22 20:07:34 +0300

    feat: add api service to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { characterApi } from './../services/character';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [characterApi.reducerPath]: characterApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(characterApi.middleware),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
