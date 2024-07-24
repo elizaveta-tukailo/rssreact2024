990bef8131945a42f89fb42e9bc9d7708bb63806
Elizaveta <lizatukailo@gmail.com>
2024-07-24 11:23:20 +0300

    feat: add query for character details

--- src/services/character.ts
+++ src/services/character.ts
@@ -1,12 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type ICard from '../interfaces/ICard';
 
+type Transform = {
+  info: {
+    count: number;
+    pages: number;
+    next: string;
+    prev: null;
+  };
+  data: ICard[];
+};
+
+interface GetCharactersQueryParams {
+  page: number;
+  search?: string;
+}
+
 export const characterApi = createApi({
   reducerPath: 'characterApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api/' }),
   endpoints: (builder) => ({
-    getCharacterByName: builder.query<ICard, string>({
-      query: (name) => `character/${name}`,
+    fetchAllCharacters: builder.query<Transform, GetCharactersQueryParams>({
+      query: ({ page, search }) => {
+        return search
+          ? `character/?page=${page}&name=${search}`
+          : `character/?page=${page}`;
+      },
+    }),
+    getCharacterDetails: builder.query({
+      query: (id) => `character/${id}`,
     }),
   }),
 });
+
+export const { useFetchAllCharactersQuery } = characterApi;
