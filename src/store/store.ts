f1c0fe702c2606477decd7e3deb7b2f22de4f070
Elizaveta <lizatukailo@gmail.com>
2024-07-24 22:55:40 +0300

    feat: add reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { characterApi } from './../services/character';
-import selectedCharactersReducer from './reducers/selectedCharacters';
+import selectedCharactersReducer from './reducers/selectedCharactersSlice';
+import characterDetailReducer from './reducers/characterDetailSlice';
+import pageCharactersReducer from './reducers/pageCharactersSlice';
 import currentPageReducer from './reducers/currentPageSlice';
 
 export const store = configureStore({
   reducer: {
     [characterApi.reducerPath]: characterApi.reducer,
     selectedCharacters: selectedCharactersReducer,
-    page: currentPageReducer,
+    characterDetail: characterDetailReducer,
+    pageCharacters: pageCharactersReducer,
+    currentPage: currentPageReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(characterApi.middleware),
