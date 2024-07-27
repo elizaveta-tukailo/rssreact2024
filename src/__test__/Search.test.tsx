5f52e770d7a6cdd89671e2fc90a41ae415c939d8
Elizaveta <lizatukailo@gmail.com>
2024-07-27 18:56:09 +0300

    test: update tests for the search component

--- src/__test__/Search.test.tsx
+++ src/__test__/Search.test.tsx
@@ -2,6 +2,10 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import Search from '../components/Search';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '../context/ThemeContext';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 const mockSetItem = jest.spyOn(Storage.prototype, 'setItem');
 const mockGetItem = jest.spyOn(Storage.prototype, 'getItem');
@@ -10,11 +14,27 @@ describe('Search', () => {
   it('Show input', () => {
     render(
       <BrowserRouter>
-        <Search />
+        <ThemeProvider>
+          <Search />
+        </ThemeProvider>
       </BrowserRouter>
     );
     const input = screen.getByPlaceholderText('Type here');
     expect(input).toBeInTheDocument();
+  });
+  it('Search component renders correctly', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Provider store={store}>
+            <ThemeProvider>
+              <Search />
+            </ThemeProvider>
+          </Provider>
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
 
@@ -26,7 +46,9 @@ describe('Testing Search component', () => {
   test('Clicking the Search button saves the entered value to the local storage', () => {
     render(
       <BrowserRouter>
-        <Search />
+        <ThemeProvider>
+          <Search />
+        </ThemeProvider>
       </BrowserRouter>
     );
     const input = screen.getByPlaceholderText('Type here') as HTMLInputElement;
@@ -40,7 +62,9 @@ describe('Testing Search component', () => {
     mockGetItem.mockImplementation(() => 'stored value');
     render(
       <BrowserRouter>
-        <Search />
+        <ThemeProvider>
+          <Search />
+        </ThemeProvider>
       </BrowserRouter>
     );
     const input = screen.getByPlaceholderText('Type here') as HTMLInputElement;
