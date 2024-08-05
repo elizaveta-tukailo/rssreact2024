5a8b3e4df5b11f7b5d1157e5fbd1130f96f14fa0
Elizaveta <lizatukailo@gmail.com>
2024-08-05 21:21:05 +0300

    refactor: add next-redux-wrapper

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,23 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { characterApi } from './../services/character';
 import selectedCharactersReducer from './reducers/selectedCharactersSlice';
 import characterDetailReducer from './reducers/characterDetailSlice';
 import pageCharactersReducer from './reducers/pageCharactersSlice';
 import currentPageReducer from './reducers/currentPageSlice';
+import { createWrapper } from 'next-redux-wrapper';
 
-export const store = configureStore({
-  reducer: {
-    [characterApi.reducerPath]: characterApi.reducer,
-    selectedCharacters: selectedCharactersReducer,
-    characterDetail: characterDetailReducer,
-    pageCharacters: pageCharactersReducer,
-    currentPage: currentPageReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(characterApi.middleware),
-});
 
-setupListeners(store.dispatch);
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const setupStore = () => {
+  return configureStore({
+    reducer: {
+      [characterApi.reducerPath]: characterApi.reducer,
+      selectedCharacters: selectedCharactersReducer,
+      characterDetail: characterDetailReducer,
+      pageCharacters: pageCharactersReducer,
+      currentPage: currentPageReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(characterApi.middleware),
+  });
+};
+
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const wrapper = createWrapper(setupStore);
+export default setupStore;
