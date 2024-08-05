45c2f6e9e53f5410bf852beebba6372b08d8af0d
Elizaveta <lizatukailo@gmail.com>
2024-08-05 11:40:10 +0300

    refactor: add layout and store wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,21 @@ import { Provider } from 'react-redux';
 import ErrorBoundary from '../components/ErrorBoundary';
 import { setupStore, wrapper } from '../store/store';
 import { ThemeProvider } from '../context/ThemeContext';
+import Layout from '../components/Layout';
 
-const store = setupStore();
 
- 
 export function App({ Component, pageProps }: AppProps) {
-    return (
-        <ErrorBoundary>
-          <Provider store={store}>
-          <ThemeProvider>
+  const { store } = wrapper.useWrappedStore(pageProps);
+  return (
+    <ErrorBoundary>
+      <Provider store={store}>
+        <ThemeProvider>
+          <Layout>
             <Component {...pageProps} />
-            </ThemeProvider>
-          </Provider>
-        </ErrorBoundary>
-      );
+          </Layout>
+        </ThemeProvider>
+      </Provider>
+    </ErrorBoundary>
+  );
 }
 export default wrapper.withRedux(App);
