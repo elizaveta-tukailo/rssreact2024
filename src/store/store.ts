8a700ac70e3f035a3c62bbcf8b5b9e1d500185bc
Elizaveta <lizatukailo@gmail.com>
2024-08-10 22:49:50 +0300

    feat: add root reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,4 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
 import { characterApi } from './../services/character';
 import selectedCharactersReducer from './reducers/selectedCharactersSlice';
 import characterDetailReducer from './reducers/characterDetailSlice';
@@ -7,15 +6,17 @@ import pageCharactersReducer from './reducers/pageCharactersSlice';
 import currentPageReducer from './reducers/currentPageSlice';
 import { createWrapper } from 'next-redux-wrapper';
 
+const rootReducer = combineReducers({
+  [characterApi.reducerPath]: characterApi.reducer,
+  selectedCharacters: selectedCharactersReducer,
+  characterDetail: characterDetailReducer,
+  pageCharacters: pageCharactersReducer,
+  currentPage: currentPageReducer,
+});
+
 export const setupStore = () => {
   return configureStore({
-    reducer: {
-      [characterApi.reducerPath]: characterApi.reducer,
-      selectedCharacters: selectedCharactersReducer,
-      characterDetail: characterDetailReducer,
-      pageCharacters: pageCharactersReducer,
-      currentPage: currentPageReducer,
-    },
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(characterApi.middleware),
   });
