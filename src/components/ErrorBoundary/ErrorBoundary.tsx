b08cacd2b7dd448e1f14439492df39ecc7e941ba
Elizaveta <lizatukailo@gmail.com>
2024-08-11 14:12:20 +0300

    fix: update component import

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import { Component, ErrorInfo, ReactNode } from 'react';
-import ErrorPage from '../../pages/error';
+import ErrorPage from '../../app/error';
 
 interface Props {
   children?: ReactNode;
