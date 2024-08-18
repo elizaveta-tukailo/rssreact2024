b28a7c8f4ea3fc8c9974568007c2281bef9e984c
Elizaveta <lizatukailo@gmail.com>
2024-08-18 11:59:10 +0300

    feat: add forms

--- src/components/HookForm/HookForm.tsx
+++ src/components/HookForm/HookForm.tsx
@@ -1,6 +1,211 @@
-import {useForm} from "react-hook-form"; 
-
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { setData } from '../../store/slices/formsDataSlice';
+import formSchema from '../../utils/validateForm';
+import AllFormFields from '../../interfaces/form';
+import { convertImage } from '../../utils/convertImage';
+import { RootState } from '../../store/store';
+import { calcPasswordStrength } from '../../utils/calcPasswordStrength';
+import { useEffect, useState } from 'react';
 
 export default function HookForm() {
-  return <div>HookForm</div>;
+  const navigate = useNavigate();
+
+  const { register, handleSubmit, formState, watch } = useForm<AllFormFields>({
+    resolver: yupResolver<AllFormFields>(formSchema),
+    mode: 'onTouched',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      name: '',
+      age: 0,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      gender: '',
+      picture: undefined,
+      country: '',
+      terms: false,
+    },
+  });
+
+  const { errors, isValid } = formState;
+  const countriesList = useSelector(
+    (state: RootState) => state.countries.countries
+  );
+  const dispatch = useDispatch();
+  const watchedPasswordInput = watch('password');
+  const [passwordStrength, setPasswordStrength] = useState(0);
+  useEffect(() => {
+    if (watchedPasswordInput) {
+      const strength = calcPasswordStrength(watchedPasswordInput);
+      setPasswordStrength(strength);
+    }
+  }, [watchedPasswordInput]);
+
+  const submit: SubmitHandler<AllFormFields> = async (data: AllFormFields) => {
+    const file = data.picture;
+    let convertedImage = '';
+    if (file instanceof File) {
+      convertedImage = (await convertImage(file)) as string;
+    } else if (file instanceof FileList) {
+      if (file.length > 0) {
+        convertedImage = (await convertImage(file[0])) as string;
+      }
+    }
+    const formData = { ...data, picture: convertedImage };
+    dispatch(setData(formData));
+    navigate('/');
+  };
+
+  return (
+    <>
+      <h1 className="title">Hook form</h1>
+      <form className="form" onSubmit={handleSubmit(submit)}>
+        <div className="input-wrap">
+          <label htmlFor="name">Name</label>
+          {errors.name ? (
+            <div className="error-block">
+              <div className="error-text">{errors.name.message}</div>
+            </div>
+          ) : null}
+          <input
+            {...register('name')}
+            type="text"
+            placeholder="name"
+            autoComplete="name"
+          />
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="age">Age</label>
+          {errors.age ? (
+            <div className="error-block">
+              <div className="error-text">{errors.age.message}</div>
+            </div>
+          ) : null}
+          <input {...register('age')} type="text" autoComplete="age" />
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="email">Email</label>
+          {errors.email ? (
+            <div className="error-block">
+              <div className="error-text">{errors.email.message}</div>
+            </div>
+          ) : null}
+          <input
+            id="email"
+            {...register('email')}
+            type="email"
+            placeholder="Email"
+            autoComplete="email"
+          />
+        </div>
+        <div className="input-wrap">
+          <label htmlFor="password">Password</label>
+          {errors.password ? (
+            <div className="error-block">
+              <div className="error-text">{errors.password.message}</div>
+            </div>
+          ) : null}
+          <div className="password-wrap">
+            <input
+              id="password"
+              {...register('password')}
+              name="password"
+              type="password"
+              placeholder="****"
+              autoComplete="password"
+            />
+            {passwordStrength && errors.password ? (
+              <div className="password-strength">
+                {[0, 1, 2, 3].map((index) => (
+                  <div
+                    key={index}
+                    className={`password-bar ${index < Number(passwordStrength) ? 'active' : ''}`}
+                  />
+                ))}
+              </div>
+            ) : null}
+          </div>
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="confirmPassword">Confirm password</label>
+          {errors.confirmPassword ? (
+            <div className="error-block">
+              <div className="error-text">{errors.confirmPassword.message}</div>
+            </div>
+          ) : null}
+          <input
+            id="confirmPassword"
+            {...register('confirmPassword')}
+            type="password"
+            placeholder="****"
+            autoComplete="password"
+          />
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="gender">Gender</label>
+          {errors.gender ? (
+            <div className="error-block">
+              <div className="error-text">{errors.gender.message}</div>
+            </div>
+          ) : null}
+          <select id="gender" {...register('gender')} autoComplete="gender">
+            <option value="">Select gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="picture">Upload picture</label>
+          {errors.picture ? (
+            <div className="error-block">
+              <div className="error-text">{errors.picture.message}</div>
+            </div>
+          ) : null}
+          <input id="picture" {...register('picture')} type="file" />
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="country">Country</label>
+          {errors.country ? (
+            <div className="error-block">
+              <div className="error-text">{errors.country.message}</div>
+            </div>
+          ) : null}
+          <input
+            id="country"
+            {...register('country')}
+            list="country-options"
+            autoComplete="country"
+          />
+          <datalist id="country-options">
+            {countriesList.map((country) => (
+              <option key={country} value={country} />
+            ))}
+          </datalist>
+        </div>
+
+        <div className="input-wrap">
+          <label htmlFor="terms">Accept Terms & Conditions</label>
+          {errors.terms ? (
+            <div className="error-block">
+              <div className="error-text">{errors.terms.message}</div>
+            </div>
+          ) : null}
+          <input id="terms" {...register('terms')} type="checkbox" />
+        </div>
+
+        <button type="submit" disabled={!isValid}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -1,174 +1,285 @@
-
-import { useRef, useState } from "react";
-import { addData } from '../../store/slices/formsDataSlice.ts';
-import FormFields from "../../interfaces/form";
-import formSchema from "../../utils/validateForm";
-import { useDispatch } from "react-redux";
-import "./UncontrolledForm.css";
-
-
-const countries = [
-    'United States',
-    'Canada',
-    'United Kingdom',
-    'Australia',
-    'Germany',
-    'France',
-    'India',
-    'China',
-    'Japan',
-    'Brazil',
-    // Add more countries as needed
-  ];
-  
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setData } from '../../store/slices/formsDataSlice';
+import AllFormFields from '../../interfaces/form';
+import formSchema from '../../utils/validateForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { convertImage } from '../../utils/convertImage';
+import { ValidationError } from 'yup';
+import { calcPasswordStrength } from '../../utils/calcPasswordStrength';
 
 export default function UncontrolledForm() {
-    const dispatch = useDispatch();
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef1 = useRef<HTMLInputElement>(null);
-    const passwordRef2 = useRef<HTMLInputElement>(null);
-    const genderRef1 = useRef<HTMLInputElement>(null);
-    const genderRef2 = useRef<HTMLInputElement>(null);
-    const pictureRef = useRef<HTMLInputElement>(null);
-    const countryRef = useRef<HTMLInputElement | null>(null);
-    const acceptRef = useRef<HTMLInputElement>(null);
-    const submitRef = useRef<HTMLButtonElement>(null);
+  const countriesList = useSelector(
+    (state: RootState) => state.countries.countries
+  );
 
-    const [filteredCountries, setFilteredCountries] = useState<string[]>([]);
-    const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+  const genderRef = useRef<HTMLSelectElement>(null);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const countryRef = useRef<HTMLInputElement>(null);
+  const termsRef = useRef<HTMLInputElement>(null);
 
+  const [nameError, setNameError] = useState<string>('');
+  const [ageError, setAgeError] = useState<string>('');
+  const [emailError, setEmailError] = useState<string>('');
+  const [passwordError, setPasswordError] = useState<string>('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string>('');
+  const [genderError, setGenderError] = useState<string>('');
+  const [fileError, setFileError] = useState<string>('');
+  const [countryError, setCountryError] = useState<string>('');
+  const [termsError, setTermsError] = useState<string>('');
 
-  const handleInputChange = () => {
-    const value = countryRef.current?.value;
-    if (value) {
-      const filtered = countries.filter(country =>
-        country.toLowerCase().startsWith(value.toLowerCase())
-      );
-      setFilteredCountries(filtered);
-      setShowDropdown(true);
-    } else {
-      setFilteredCountries([]);
-      setShowDropdown(false);
-    }
-  };
+  const [passwordStrength, setPasswordStrength] = useState<string>('');
 
-  const handleCountrySelect = (country:string) => {
-    if (countryRef.current) {
-        countryRef.current.value = country;
-    }
-    setShowDropdown(false);
-  };
-
-  const handleBlur = () => {
-    setTimeout(() => setShowDropdown(false), 100); 
-  };
-
-  const handleFocus = () => {
-    //if (countryRef.current?.value) {
-      setShowDropdown(true);
-    //}
-    console.log("focus");
-  };
-
-
-  const handleSubmit = (event : React.FormEvent<HTMLFormElement>):void => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    //if (submitRef.current) {submitRef.current.disabled = true; }
-    
 
-    const formData: FormFields = {
-        name: nameRef.current?.value || '',
-        age: Number(ageRef.current?.value) || 0,
-        email: emailRef.current?.value || '',
-        password: passwordRef1.current?.value || '',
-        gender: genderRef1.current?.checked ? "Male" : "Female",
-        country: 1,
-        picture: pictureRef.current?.value || '',
+    let convertedFile = '';
+    const pictureFile = fileRef.current?.files?.[0];
+    if (pictureFile) {
+      convertedFile = (await convertImage(pictureFile)) as string;
+    }
+    const formData: AllFormFields = {
+      name: nameRef.current?.value,
+      age: Number(ageRef.current?.value),
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value,
+      confirmPassword: confirmPasswordRef.current?.value,
+      gender: genderRef.current?.value,
+      country: countryRef.current?.value,
+      picture: pictureFile,
+      terms: termsRef.current?.checked,
     };
-    const errors = formSchema.validate(formData);
-    console.log(errors);
-    
-    console.log(errors);
-    dispatch(addData(formData));
-    //const expenseDetails = Object.fromEntries(expense);
-    //
-    //console.log(pictureRef.current.files[0].name);
-    //const form = e.target;
-   //console.log(formData);
+    if (formData.password) {
+      const strength = calcPasswordStrength(formData.password);
+      setPasswordStrength(String(strength));
+    }
 
+    const formDataToSave = { ...formData, picture: convertedFile };
+    delete formDataToSave.confirmPassword;
+    try {
+      await formSchema.validate(formData, { abortEarly: false });
+      dispatch(setData(formDataToSave));
+      if (nameRef.current) {
+        nameRef.current.value = '';
+      }
+      if (ageRef.current) {
+        ageRef.current.value = '';
+      }
+      if (emailRef.current) {
+        emailRef.current.value = '';
+      }
+      if (passwordRef.current) {
+        passwordRef.current.value = '';
+      }
+      if (confirmPasswordRef.current) {
+        confirmPasswordRef.current.value = '';
+      }
+      if (genderRef.current) {
+        genderRef.current.value = '';
+      }
+      if (termsRef.current) {
+        termsRef.current.checked = false;
+      }
+      if (fileRef.current) {
+        fileRef.current.value = '';
+      }
+      if (countryRef.current) {
+        countryRef.current.value = '';
+      }
+
+      navigate('/');
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        const errors = error.inner.reduce(
+          (acсumulator, validationError) => ({
+            ...acсumulator,
+            [validationError.path as keyof typeof formData]:
+              validationError.message,
+          }),
+          {} as Record<keyof typeof formData, string>
+        );
+
+        setNameError(errors.name || '');
+        setAgeError(errors.age || '');
+        setEmailError(errors.email || '');
+        setPasswordError(errors.password || '');
+        setConfirmPasswordError(errors.confirmPassword || '');
+        setGenderError(errors.gender || '');
+        setFileError(errors.picture || '');
+        setCountryError(errors.country || '');
+        setTermsError(errors.terms || '');
+      }
+    }
   };
 
   return (
-      <form method="post" onSubmit={(e) => handleSubmit(e)}>
-        <div>
-            <label htmlFor="name">Name:</label>
-            <input name="name" type="text" placeholder="name" ref={nameRef} />
-            {/* {errors.email && <p>Please enter a valid email</p>} */}
+    <>
+      <h1 className="title">Uncontrolled form</h1>
+      <form className="form" method="post" onSubmit={(e) => handleSubmit(e)}>
+        <div className="input-wrap">
+          <label htmlFor="name">Name:</label>
+          {nameError ? (
+            <div className="error-block">
+              <div className="error-text">{nameError}</div>
+            </div>
+          ) : null}
+          <input
+            name="name"
+            type="text"
+            placeholder="name"
+            autoComplete="name"
+            ref={nameRef}
+          />
         </div>
-        <div>
-            <label htmlFor="age">Age:</label>
-            <input name="age" type="text" placeholder="age" ref={ageRef}/>
+        <div className="input-wrap">
+          <label htmlFor="age">Age:</label>
+          {ageError ? (
+            <div className="error-block">
+              <div className="error-text">{ageError}</div>
+            </div>
+          ) : null}
+          <input
+            id="age"
+            name="age"
+            type="text"
+            placeholder="age"
+            autoComplete="age"
+            ref={ageRef}
+          />
         </div>
-        <div>
-            <label htmlFor="email">Email:</label>
-            <input name="email" type="email" placeholder="email" ref={emailRef}/>
+        <div className="input-wrap">
+          <label htmlFor="email">Email:</label>
+          {emailError ? (
+            <div className="error-block">
+              <div className="error-text">{emailError}</div>
+            </div>
+          ) : null}
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="email"
+            autoComplete="email"
+            ref={emailRef}
+          />
         </div>
-        <div>
-            <label htmlFor="password">Password:</label>
-            <input name="password" type="password" placeholder="****" ref={passwordRef1}/>
+        <div className="input-wrap">
+          <label htmlFor="password">Password:</label>
+          {passwordError ? (
+            <div className="error-block">
+              <div className="error-text">{passwordError}</div>
+            </div>
+          ) : null}
+          <div className="password-wrap">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="****"
+              autoComplete="new-password"
+              ref={passwordRef}
+            />
+            {passwordStrength && passwordError ? (
+              <div className="password-strength">
+                {[0, 1, 2, 3].map((index) => (
+                  <div
+                    key={index}
+                    className={`password-bar ${index < Number(passwordStrength) ? 'active' : ''}`}
+                  />
+                ))}
+              </div>
+            ) : null}
+          </div>
         </div>
-        <div>
-            <label htmlFor="password2">Confirm password:</label>
-            <input name="password2" type="password" placeholder="****" ref={passwordRef2}/>
+        <div className="input-wrap">
+          <label htmlFor="confirm_password">Confirm password:</label>
+          {confirmPasswordError ? (
+            <div className="error-block">
+              <div className="error-text">{confirmPasswordError}</div>
+            </div>
+          ) : null}
+          <input
+            id="confirm_password"
+            name="confirm_password"
+            type="password"
+            placeholder="****"
+            autoComplete="confirmPassword"
+            ref={confirmPasswordRef}
+          />
         </div>
-        <div>
-            <label>
-            Gender
-            <input type="radio" name="gender" value="male" id="male" ref={genderRef1}/>
-            <label htmlFor="regular">Male</label>
-            <input type="radio" name="gender" value="female" id="female" ref={genderRef2}/>
-            <label htmlFor="medium">Female</label>
-            </label>
+        <div className="input-wrap">
+          <label htmlFor="gender">Gender</label>
+          {genderError ? (
+            <div className="error-block">
+              <div className="error-text">{genderError}</div>
+            </div>
+          ) : null}
+          <select
+            id="gender"
+            name="gender"
+            autoComplete="gender"
+            ref={genderRef}
+          >
+            <option value="">Select gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
         </div>
-        <div>
-        <label htmlFor="file">Upload Picture</label>
-        <input
-          type="file"
-          name="file"
-          id="upload-file"
-          placeholder="Enter Upload File"
-          ref={pictureRef}
-        />
+        <div className="input-wrap">
+          <label htmlFor="upload-file">Upload picture file</label>
+          {fileError ? (
+            <div className="error-block">
+              <div className="error-text">{fileError}</div>
+            </div>
+          ) : null}
+          <input
+            type="file"
+            name="picture"
+            id="upload-file"
+            placeholder="Enter Upload File"
+            autoComplete="file"
+            ref={fileRef}
+          />
         </div>
-        <div>
-        <input
-        ref={countryRef}
-        type="text"
-        onChange={handleInputChange}
-        onBlur={handleBlur}
-        onFocus={handleFocus}
-        placeholder="Select a country"
-      />
-      {showDropdown}
-      {/* && filteredCountries.length > 0 */}
-      {showDropdown  && (
-        <ul className="dropdown-list">
-          {filteredCountries.map((country, index) => (
-            <li key={index} onClick={() => handleCountrySelect(country)}>
-              {country}
-            </li>
-          ))}
-        </ul>
-      )}
+        <div className="input-wrap">
+          <label htmlFor="country">Country</label>
+          {countryError ? (
+            <div className="error-block">
+              <div className="error-text">{countryError}</div>
+            </div>
+          ) : null}
+          <input
+            id="country"
+            name="country"
+            list="country-options"
+            autoComplete="country"
+            ref={countryRef}
+          />
+          <datalist id="country-options">
+            {countriesList.map((country) => (
+              <option key={country} value={country} />
+            ))}
+          </datalist>
         </div>
-        <div>
-            <label><input name="rules" type="checkbox" placeholder="" ref={acceptRef}/>I accept rules</label>
+        <div className="input-wrap">
+          <label htmlFor="terms">Accept Terms & Conditions</label>
+          {termsError ? (
+            <div className="error-block">
+              <div className="error-text">{termsError}</div>
+            </div>
+          ) : null}
+          <input id="terms" ref={termsRef} name="terms" type="checkbox" />
         </div>
-        <button type="submit" ref={submitRef}>
-          Submit
-        </button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 }
