e4fc2fbb4ed3106824b25be8eceb30161612151f
Elizaveta <lizatukailo@gmail.com>
2024-08-18 08:55:48 +0300

    feat: add interface

--- src/interfaces/form.ts
+++ src/interfaces/form.ts
@@ -1,9 +1,22 @@
 export default interface FormFields {
-  name: string;
-  age: number;
-  email: string;
-  password: string;
-  gender: string;
-  picture: string;
-  country: number;
+  name?: string;
+  age?: number;
+  email?: string;
+  password?: string;
+  gender?: string;
+  picture?: File | FileList | string;
+  country?: string;
+  terms?: boolean;
+}
+
+export default interface AllFormFields {
+  name?: string;
+  age?: number;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  gender?: string;
+  picture?: File | FileList | string;
+  country?: string;
+  terms?: boolean;
 }
